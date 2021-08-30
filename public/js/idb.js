37aec41b5246dcbaad6baa7f2313189d0d3a222e
Stanley <aynbunda@gmail.com>
2021-08-30 16:38:08 -0400

    records db

--- public/js/idb.js
+++ public/js/idb.js
@@ -25,3 +25,65 @@ request.onerror = function(event) {
    
     console.log(event.target.errorCode);
 };
+
+
+function saveRecord(record) {
+
+    
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    
+    const  budgetObjectStore = transaction.objectStore('new_transaction');
+
+    
+    budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+
+   
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+
+        
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                   
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+                    
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+                    
+                    budgetObjectStore.clear();
+
+                    alert('All saved transactions has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
