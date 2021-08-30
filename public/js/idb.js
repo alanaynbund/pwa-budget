89e33eb704abcbff83e865676ac64634ca98f718
Stanley <aynbunda@gmail.com>
2021-08-30 15:23:18 -0400

    db

---
+++ public/js/idb.js
@@ -0,0 +1,27 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+
+    const db = event.target.result;
+
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+// upon a successful
+request.onsuccess = function(event) {
+
+   
+    db = event.target.result;
+
+    
+    if (navigator.onLine) {
+        
+    }
+};
+
+request.onerror = function(event) {
+   
+    console.log(event.target.errorCode);
+};
